0069d0a8e9081f87da454e4a6541e416f26f6f1a
reynsu <srzrnl92h03z504m@studenti.unical.it>
2022-04-08 14:27:33 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
     // console.log("render");
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           onChangeHandler={onSearchChange}
           placeholder="search monster"
